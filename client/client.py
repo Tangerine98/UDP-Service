9eba14a974d2d0cf1cff799edc8b177355674d0b
Tangerine98 <menonnandini98@gmail.com>
2019-03-17 21:43:54 +0530

    Implement menu

--- client/client.py
+++ client/client.py
@@ -1,22 +1,47 @@
 import socket
+import sys
 import base64
 
 ip_addr = '127.0.0.1'
 udp_port = 8000
-max_bytes = 10000000
+max_bytes = 10240
+
+def menu() :
+    print("MENU:-")
+    print("\t1. List all files")
+    print("\t2. Get a file")
+    print("\t3. Exit")
+    op = int(input("Enter your choice: "))
+    return op
 
 if __name__ == '__main__' :
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    msg = input("Enter file name: ")
-    client.sendto(bytes(msg,'utf-8'), (ip_addr, udp_port))
+    while True:
+        op = menu()
+        client.sendto(bytes(str(op),'utf-8'), (ip_addr,udp_port))
+        if op == 2:
+            msg = input("\nEnter file name: ")
+            client.sendto(bytes(msg,'utf-8'), (ip_addr, udp_port))
 
-    data, addr = client.recvfrom(max_bytes)
-    try:
-        file_content = base64.b64decode(data)
+            data, addr = client.recvfrom(max_bytes)
+            try:
+                file_content = base64.b64decode(data)
 
-        file = open(msg, 'wb')
-        file.write(file_content)
-    except:
-        message = str(data.decode())
-        print(message)
+                file = open(msg, 'wb')
+                file.write(file_content)
+                print("File {} downloaded".format(msg))
+            except:
+                message = str(data.decode())
+                print(message)
+
+        elif op == 3:
+            client.sendto(bytes("Stop",'utf-8'), (ip_addr,udp_port))
+            print('\nTerminating connection..')
+            sys.exit()
+
+        else:
+            print('\nInvalid option\n')
+
+
+        
--- server/server.py
+++ server/server.py
@@ -1,31 +1,45 @@
 import socket
 import os
+import sys
 import base64
 
 ip_addr = '127.0.0.1'
 udp_port = 8000
-max_bytes = 10000000
+max_bytes = 10240
 
 if __name__ == '__main__' :
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server.bind((ip_addr, udp_port))
     print('Server started at ',ip_addr,':',udp_port)
+    print('Waiting for connection..')
     while True:
-        data, addr = server.recvfrom(max_bytes)
-        file_name = str(data.decode())
+
+        op, addr = server.recvfrom(max_bytes)
+        #print("\nConnected to client at port: {}".format(addr[1]))
+        op = int(str(op.decode()))
+
+        if op == 2:
+            data, addr = server.recvfrom(max_bytes)
+            file_name = str(data.decode())
+            print("\nRequested File : ",file_name)
+            file_name = 'images/'+str(data.decode())
             
-        exists = os.path.isfile(file_name)
-        if exists:
-            file_size = os.path.getsize(file_name)
-            if file_size < 10000000 :
-                file = open(file_name, 'rb')
-                file_content = file.read()
-                base64_format = base64.encodestring(file_content)
-                server.sendto(base64_format, addr)
-            else :
-                return_string = "File can't be transferred since it has size greater than 10MB"
-                message = bytes(return_string,'utf-8')
+            exists = os.path.isfile(file_name)
+            if exists:
+                file_size = os.path.getsize(file_name)
+                if file_size < max_bytes :
+                    file = open(file_name, 'rb')
+                    file_content = file.read()
+                    base64_format = base64.encodestring(file_content)
+                    server.sendto(base64_format, addr)
+                else :
+                    return_string = "File can't be transferred since it has size greater than 10KB"
+                    message = bytes(return_string,'utf-8')
+                    server.sendto(message, addr)
+            else:
+                message = bytes("File doesn't exist",'utf-8')
                 server.sendto(message, addr)
-        else:
-            message = bytes("File doesn't exist",'utf-8')
-            server.sendto(message, addr)
+
+        elif op == 3:
+            print('\nConnection terminated')
+            sys.exit()
